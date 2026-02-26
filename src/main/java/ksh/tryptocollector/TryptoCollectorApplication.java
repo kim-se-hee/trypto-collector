0724b78756970697fd217aa672baf818f082a44e
kim-se-hee <saree98@naver.com>
2026-02-27 03:02:11 +0900

    init: 최초 커밋

---
+++ src/main/java/ksh/tryptocollector/TryptoCollectorApplication.java
@@ -0,0 +1,13 @@
+package ksh.tryptocollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryptoCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryptoCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ksh/tryptocollector/TryptoCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.tryptocollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryptoCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
